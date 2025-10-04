01c97b9d74892de3a2ce4b06123761f01a057bb5
Codinglynd <ethanrocks500@proton.me>
2025-10-04 15:53:46 -0500

    commit

---
+++ input/if_statments/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i;
+
+    cout << "What is i? ";
+    cin >> i;
+
+    if(i > 0){
+        i+=2;
+    }
+
+    cout << i << '\n';
+
+    return 0;
+}
