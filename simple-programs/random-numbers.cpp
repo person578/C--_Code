8d7951cc2d057428fad1c75d5020bdf1b0a4192f
Codinglynd <ethanrocks500@proton.me>
2025-10-13 09:48:04 -0500

    made 3 variables that can be any number 1-6 and then I print those numbers

--- simple-programs/random-numbers.cpp
+++ simple-programs/random-numbers.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main()
 {
 
+    srand(time(NULL));
 
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
 
     return 0;
 }
