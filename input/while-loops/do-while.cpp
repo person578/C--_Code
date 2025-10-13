139a7f1916a1d9a40ea62c53c722375bf9fb9500
Codinglynd <ethanrocks500@proton.me>
2025-10-12 22:30:47 -0500

    changed the name of the while-statments folder to while-loops

---
+++ input/while-loops/do-while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int number;
+
+    do {
+        cout << "Enter a postive # this time ";
+        cin >> number;
+    }while(number < 0);
+
+    cout << "The number is " << number << endl;
+
+    return 0;
+}
