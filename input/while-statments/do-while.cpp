3ddf001beae2999b5e3386aa0a26407b4fdd9689
Codinglynd <ethanrocks500@proton.me>
2025-10-12 20:10:55 -0500

    added a int of number and a while loop for if number is less than zero ask for the number to be a positive # this time

--- input/while-statments/do-while.cpp
+++ input/while-statments/do-while.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main()
 {
 
+    int number;
+
+    while(number < 0){
+        cout << "Enter a postive # this time ";
+        cin >> number;
+    }
+
     
 
     return 0;
