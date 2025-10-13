77cbec325482b6981b4cd299ac262e93a2ba2189
Codinglynd <ethanrocks500@proton.me>
2025-10-13 09:24:41 -0500

    got rid of the if statment instead when you get out of the inner for loop it will print a new line

--- input/loops/for-loops/nested-loops.cpp
+++ input/loops/for-loops/nested-loops.cpp
@@ -7,10 +7,8 @@ int main()
     for(int i = 1; i <= 3; i++){
         for(int j = 1; j <= 10; j++){
             cout << j << ' ';
-            if(j == 10){
-                cout << endl;
-            }
         }
+        cout << '\n';
     }
 
     
