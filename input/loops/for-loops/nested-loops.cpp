66dba8b40a2083406c3ff15dbf1e2d9b3bab4351
Codinglynd <ethanrocks500@proton.me>
2025-10-13 09:16:46 -0500

    nested the first for loop in another for loop that does the original output 3 times

--- input/loops/for-loops/nested-loops.cpp
+++ input/loops/for-loops/nested-loops.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main()
 {
 
-    
-
-    for(int i = 1; i <= 10; i++){
-        cout << i << ' ';
+    for(int i = 1; i <= 3; i++){
+        for(int i = 1; i <= 10; i++){
+            cout << i << ' ';
+        }
     }
+
+    
 
     return 0;
 }
