622462fafc9a5fd22476b9a21fad66d95ed8b4dc
Codinglynd <ethanrocks500@proton.me>
2025-10-26 14:10:55 -0500

    made a practice banking program

---
+++ input/banking-program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+    double balance = 0;
+    int choice = 0;
+
+    cout << "Enter your choice ";
+    cin >> choice;
+
+    
+
+    return 0;
+}
