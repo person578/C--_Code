8c90f1cfbedc39464888e8e6567b49677f74438c
Codinglynd <ethanrocks500@proton.me>
2025-10-26 14:32:29 -0500

    put all previous and new code in a do while loop for choice being not equal to 4 also added a switch for each option

--- input/banking-program.cpp
+++ input/banking-program.cpp
@@ -9,16 +9,28 @@ int main()
 {
     double balance = 0;
     int choice = 0;
+    do{
+        cout << "******************\n";
+        cout << "Enter your choice\n";
+        cout << "******************\n";
+        cout << "1. Show Balance\n";
+        cout << "2. Deposit Money\n";
+        cout << "3. Withdraw Money\n";
+        cout << "4. Exit\n\n";
+        cout << "What is your choice? ";
+        cin >> choice;
 
-    cout << "******************\n";
-    cout << "Enter your choice\n";
-    cout << "******************\n";
-    cout << "1. Show Balance\n";
-    cout << "2. Deposit Money\n";
-    cout << "3. Withdraw Money\n";
-    cout << "4. Exit\n\n";
-    cout << "What is your choice? ";
-    cin >> choice;
-
+        switch(choice){
+            case 1: showBalance(balance);
+                break;
+            case 2: balance += deposit();
+                break;
+            case 3: balance -= withdraw(balance);
+                break;
+            case 4: cout << "Thanks for visiting\n";
+                break;
+            default: cout << "Invalid response";
+        }
+    }while(choice != 4);
     return 0;
 }
