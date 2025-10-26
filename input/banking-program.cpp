e55c2d8d11afd0e0602cfd788dd1b9bd068bb210
Codinglynd <ethanrocks500@proton.me>
2025-10-26 14:24:45 -0500

    added input for the variable choice with show balance for 1 deposit for 2 withdraw for 3 and exit for 4 it then asks what your choice is

--- input/banking-program.cpp
+++ input/banking-program.cpp
@@ -10,10 +10,15 @@ int main()
     double balance = 0;
     int choice = 0;
 
-    cout << "Enter your choice ";
+    cout << "******************\n";
+    cout << "Enter your choice\n";
+    cout << "******************\n";
+    cout << "1. Show Balance\n";
+    cout << "2. Deposit Money\n";
+    cout << "3. Withdraw Money\n";
+    cout << "4. Exit\n\n";
+    cout << "What is your choice? ";
     cin >> choice;
-
-    
 
     return 0;
 }
