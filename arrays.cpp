b5d622f07ab4bd6bd181a288ab9644252416a347
Codinglynd <ethanrocks500@proton.me>
2025-10-26 22:33:23 -0500

    added a file for learning arrays that makes a array for cars that has Honda Chevy Corvette and Mustang it then shows every one of those

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  string cars[] = {"Honda", "Chevy", "Corvette", "Mustang"};
+
+  cout << cars[0] << '\n';
+  cout << cars[1] << '\n';
+  cout << cars[2] << '\n';
+  cout << cars[3] << '\n';
+
+  return 0;
+}
